f1373c668879af5b40ddd4e33182d6fa590aee61
Marius Storhaug <marstor@hotmail.com>
2025-02-02 09:53:33 +0100

    Add tests for Search-GUID and Test-IsGUID functions; remove obsolete PSModuleTest

---
+++ tests/GUID.Tests.ps1
@@ -0,0 +1,23 @@
+﻿Describe 'Functions' {
+    Context 'Function: Search-GUID' {
+        It 'Search-GUID finds a GUID from a string' {
+            $GUID = 'Test this 123e4567-e89b-12d3-a456-426655440000' | Search-GUID
+            $GUID | Should -Be '123e4567-e89b-12d3-a456-426655440000'
+        }
+        It 'Search-GUID returns null if a GUID is not found' {
+            $GUID = 'Test this 123e4567-e85440000' | Search-GUID
+            $GUID | Should -BeNullOrEmpty
+        }
+    }
+
+    Context 'Function: Search-GUID' {
+        It 'Search-GUID returns true if a GUID is found' {
+            $GUID = '123e4567-e89b-12d3-a456-426655440000' | Test-IsGUID
+            $guid | Should -BeTrue
+        }
+        It 'Search-GUID returns false if a GUID is not found' {
+            $GUID = 'Test this 123e4567-e85440000' | Test-IsGUID
+            $guid | Should -BeFalse
+        }
+    }
+}
